57bfcb8d23acf8e45149eebe6d2ed77132cc1335
GEO FRANK <75298526+Geofrank1996@users.noreply.github.com>
2023-09-26 00:46:36 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors(
     {
-        origin:[""],
+        origin:["https://mernclient-six.vercel.app"],
         methods:["POST","GET"],
         credentials:true
     }
